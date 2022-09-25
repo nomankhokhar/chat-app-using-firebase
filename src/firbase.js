1e534157d38fc21cb79ce6ea031c4c5f209e632a
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2022-09-25 18:05:29 +0500

    Put your Firebase Crediential

--- src/firbase.js
+++ src/firbase.js
@@ -5,12 +5,7 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBN227PRqP0XwF0XeCV6Q4e45E_G4Rk3hY",
-  authDomain: "chat-app-68228.firebaseapp.com",
-  projectId: "chat-app-68228",
-  storageBucket: "chat-app-68228.appspot.com",
-  messagingSenderId: "445088784966",
-  appId: "1:445088784966:web:a1d3e487dcd9c0c217e047"
+
 };
 
 export const app = initializeApp(firebaseConfig);
